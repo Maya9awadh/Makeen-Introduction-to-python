457195f10add2efc22f65dd0f9f63a41b05e3dd5
Maya9awadh <125116838+Maya9awadh@users.noreply.github.com>
2023-05-09 14:11:43 +0400

    Add files via upload

---
+++ price_vat.py
@@ -0,0 +1,12 @@
+#program to find total cost of purchase with addition vat
+
+#initialize variable
+amount_of_purchase=10
+
+addition = amount_of_purchase * 0.05
+
+#calculate total purchase
+total_purchse= amount_of_purchase + addition
+
+#print the total purchase
+print("Total purchase is: ",total_purchse)
