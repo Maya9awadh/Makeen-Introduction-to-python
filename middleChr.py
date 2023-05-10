6717d6ac2f69f1ef1f0ba40859aced4c8778d78b
Maya9awadh <125116838+Maya9awadh@users.noreply.github.com>
2023-05-10 12:34:09 +0400

    Add files via upload

---
+++ middleChr.py
@@ -0,0 +1,15 @@
+
+#get string from user
+string=input("enter string: ")
+
+a=len(string)
+
+middle=""
+
+if a%2==0:
+
+    middle=string[a//2] + string[(a//2)-1]
+else:
+    
+    middle=string[((a+1)//2)-1]
+print("middle char is: ",middle)
